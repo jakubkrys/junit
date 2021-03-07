755ace3c9f6800b54c1d6ca0a9690728870babee
Jakub <jakubkrys@gmail.com>
2021-03-07 13:53:47 +0100

    #1 test poprawności dnia

---
+++ src/main/java/pl/jakubkrys/junit/PeselValidator.java
@@ -0,0 +1,22 @@
+package pl.jakubkrys.junit;
+
+public final class PeselValidator {
+
+    public static boolean peselLengthValidation(String pesel) {
+       return pesel.length() == 11;
+        /*
+       lub
+
+       if(pesel.length() == 11){
+            return true;
+        } else {
+            return false;
+        }*/
+    }
+
+    public static boolean peselDayValidation(String pesel) {
+        String day = pesel.substring(4,6);
+        int dayNumber = Integer.parseInt(day);
+        return dayNumber >= 1 && dayNumber <= 31;
+    }
+}
--- src/test/java/pl/jakubkrys/junit/PeselValidatorTest.java
+++ src/test/java/pl/jakubkrys/junit/PeselValidatorTest.java
@@ -4,39 +4,39 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ValidatorTest {
+public class PeselValidatorTest {
 
     @Test
-    public void isNotValidIfLength4(){
+    public void isNotValidLengthIf4(){
         //given
         String pesel = "1511";
 
         //when
-        boolean isValid = Validator.peselValidation(pesel);
+        boolean isValid = PeselValidator.peselLengthValidation(pesel);
 
         //then
         assertFalse(isValid);
     }
 
     @Test
-    public void isValidIfLength11(){
+    public void isValidLengthIf11(){
         //given
-        String pesel = "15119952648";
+        String pesel = "15113152648";
 
         //when
-        boolean isValid = Validator.peselValidation(pesel);
+        boolean isValid = PeselValidator.peselLengthValidation(pesel);
 
         //then
         assertTrue(isValid);
     }
 
     @Test
-    public void isNotValidDayIfEquals99(){
+    public void isNotValidDayIfEquals32(){
         //given
-        String pesel = "15119952648";
+        String pesel = "15113252648";
 
         //when
-        boolean isValid = Validator.peselValidation(pesel);
+        boolean isValid = PeselValidator.peselDayValidation(pesel);
 
         //then
         assertFalse(isValid);
@@ -46,10 +46,10 @@ public class ValidatorTest {
     @Test
     public void isValidDayIfFrom1To31(){
         //given
-        String pesel = "15110152648";
+        String pesel = "15113152648";
 
         //when
-        boolean isValid = Validator.peselValidation(pesel);
+        boolean isValid = PeselValidator.peselDayValidation(pesel);
 
         //then
         assertTrue(isValid);
