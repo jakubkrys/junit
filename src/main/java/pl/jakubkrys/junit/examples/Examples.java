676b5b9b93b9e00bc6771f4483a5977f8cbbc1db
Jakub <jakubkrys@gmail.com>
2021-03-30 18:35:32 +0200

    #7 przygotowanie testu isAdult

--- src/main/java/pl/jakubkrys/junit/examples/Examples.java
+++ src/main/java/pl/jakubkrys/junit/examples/Examples.java
@@ -7,6 +7,6 @@ public class Examples {
     }
 
     public static boolean isAdult(int age) {
-        return age > 18;
+        return age >= 18;
     }
 }
--- src/test/java/pl/jakubkrys/junit/examples/ExamplesTest.java
+++ src/test/java/pl/jakubkrys/junit/examples/ExamplesTest.java
@@ -7,12 +7,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ExamplesTest {
 
-    @ParameterizedTest (name = "Person with age {0} is not adult")
-    @ValueSource (ints = {11, 2, 15, 5, 10, 14, 17})
-    public void isNotAdult(int age){
+    @ParameterizedTest(name = "Person with age {0} is not adult")
+    @ValueSource(ints = {11, 2, 15, 5, 10, 14, 17})
+    public void isNotAdult(int age) {
         //when
-boolean isAdult = Examples.isAdult(age);
+        boolean isAdult = Examples.isAdult(age);
         //then
         assertFalse(isAdult);
+    }
+
+    @ParameterizedTest(name = "Person with age {0} is adult")
+    @ValueSource(ints = {18, 35, 22, 89, 109})
+    public void isAdult(int age) {
+        //when
+        boolean isAdult = Examples.isAdult(age);
+        //then
+        assertTrue(isAdult);
     }
 }
